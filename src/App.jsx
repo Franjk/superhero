b74b90842d6245a408ad7d70c640d3d4be7d0818
Francisco <fkarkovich@gmail.com>
2021-06-19 11:33:38 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LogInForm from './components/LogInForm/LogInForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LogInForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
