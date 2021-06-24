b3acffa44c5cd8f7624836b90dfd5e99405af525
Francisco <fkarkovich@gmail.com>
2021-06-24 20:50:25 -0300

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-// import Team from './components/Team/Team';
+import { Route, Switch } from 'react-router-dom';
 import HeroSearchPage from './components/HeroSearchPage/HeroSearchPage';
+import LogInForm from './components/LogInForm/LogInForm';
+import Team from './components/Team/Team';
 import './App.css';
-// import LogInForm from './components/LogInForm/LogInForm';
+import HeroDetails from './components/HeroDetails/HeroDetails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HeroSearchPage />
+        Header
       </header>
+      <main>
+        <Switch>
+          <Route path="/team">
+            <Team />
+          </Route>
+          <Route path="/login">
+            <LogInForm />
+          </Route>
+          <Route path="/search">
+            <HeroSearchPage />
+          </Route>
+          <Route path="/hero/:id">
+            <HeroDetails />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
